465971c14506819afece8f5cab6878814f483b50
zzzzzzzs <1443539042@qq.com>
2021-07-02 22:19:27 +0800

    flink-1.12.0-comment init

---
+++ flink-yarn/src/main/java/org/apache/flink/yarn/cli/AbstractYarnCli.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.yarn.cli;
+
+import org.apache.flink.client.cli.AbstractCustomCommandLine;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.DeploymentOptions;
+import org.apache.flink.yarn.configuration.YarnConfigOptions;
+import org.apache.flink.yarn.executors.YarnJobClusterExecutor;
+import org.apache.flink.yarn.executors.YarnSessionClusterExecutor;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+abstract class AbstractYarnCli extends AbstractCustomCommandLine {
+
+	public static final String ID = "yarn-cluster";
+
+	protected final Option applicationId;
+
+	protected final Option addressOption =
+		new Option("m", "jobmanager", true, "Set to " + ID + " to use YARN execution mode.");
+
+	protected final Configuration configuration;
+
+	protected AbstractYarnCli(Configuration configuration, String shortPrefix, String longPrefix) {
+		this.configuration = configuration;
+		this.applicationId = new Option(shortPrefix + "id", longPrefix + "applicationId", true, "Attach to running YARN session");
+	}
+
+	@Override
+	public boolean isActive(CommandLine commandLine) {
+		final String jobManagerOption = commandLine.getOptionValue(addressOption.getOpt(), null);
+		/*TODO ID是固定的字符串 "yarn-cluster"*/
+		final boolean yarnJobManager = ID.equals(jobManagerOption);
+		/*TODO 判断是否存在 Yarn Session对应的 AppID*/
+		// 判断输入的是否有id
+		final boolean hasYarnAppId = commandLine.hasOption(applicationId.getOpt())
+			// 从yarn上找id
+			|| configuration.getOptional(YarnConfigOptions.APPLICATION_ID).isPresent();
+		final boolean hasYarnExecutor = YarnSessionClusterExecutor.NAME.equalsIgnoreCase(configuration.get(DeploymentOptions.TARGET))
+			|| YarnJobClusterExecutor.NAME.equalsIgnoreCase(configuration.get(DeploymentOptions.TARGET));
+		/*TODO -m yarn-cluster || yarn有appID，或者命令行指定了 || 执行器是yarn的*/
+		return hasYarnExecutor || yarnJobManager || hasYarnAppId;
+	}
+
+	@Override
+	public void addGeneralOptions(Options baseOptions) {
+		super.addGeneralOptions(baseOptions);
+		baseOptions.addOption(applicationId);
+		baseOptions.addOption(addressOption);
+	}
+
+	@Override
+	public String getId() {
+		return ID;
+	}
+}
